8ac808f5828973b7c814805cf5a90144d194091d
SangHuPark <tkdgn5408407@gmail.com>
2025-07-26 13:20:15 +0900

    refactor: article cache manager 분리

--- backend/newsseug/src/main/java/com/a301/newsseug/external/caffeine/manager/ArticleCacheManager.java
+++ backend/newsseug/src/main/java/com/a301/newsseug/external/caffeine/manager/ArticleCacheManager.java
@@ -3,26 +3,38 @@ package com.a301.newsseug.external.caffeine.manager;
 import com.a301.newsseug.domain.article.model.entity.Article;
 import com.a301.newsseug.domain.article.repository.ArticleRepository;
 import com.a301.newsseug.external.caffeine.type.CacheType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ArticleCacheManager extends BaseCacheManager<Article, Long> {
+import java.util.Objects;
+import java.util.Set;
 
+@Service
+@RequiredArgsConstructor
+public class ArticleCacheManager {
+
+    private final CacheManager cacheManager;
     private final ArticleRepository articleRepository;
 
-    public ArticleCacheManager(CacheManager cacheManager, ArticleRepository articleRepository) {
-        super(cacheManager);
-        this.articleRepository = articleRepository;
+    @Cacheable(value = "article", key = "#id")
+    public Article getCached(Long id) {
+        return articleRepository.getOrThrow(id);
     }
 
-    @Override
+    public void evict(Long id) {
+        Objects.requireNonNull(cacheManager.getCache(getCacheName())).evict(id);
+    }
+
+    public void evictBatch(Set<String> ids) {
+        Cache cache = Objects.requireNonNull(cacheManager.getCache(getCacheName()));
+        ids.forEach(cache::evict);
+    }
+
     protected String getCacheName() {
         return CacheType.ARTICLE.getName();
     }
 
-    @Override
-    protected Article loadFromRepository(Long id) {
-        return articleRepository.getOrThrow(id);
-    }
 }
